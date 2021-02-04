2a60aa511c99265158b3e2c9f6bb5b8d98fe4a9e
Qais Yousuf <mr.qais.yousuf@gmail.com>
2021-02-04 19:42:52 +0100

    ConnectionStringCreated

--- OnlineTrainingWeb/App_Start/IdentityConfig.cs
+++ OnlineTrainingWeb/App_Start/IdentityConfig.cs
@@ -1,16 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Data.Entity;
-using System.Linq;
-using System.Security.Claims;
-using System.Threading.Tasks;
-using System.Web;
+﻿using Data;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
 using Microsoft.AspNet.Identity.Owin;
 using Microsoft.Owin;
 using Microsoft.Owin.Security;
-using OnlineTrainingWeb.Models;
+using Model;
+using System;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
 
 namespace OnlineTrainingWeb
 {
--- OnlineTrainingWeb/App_Start/Startup.Auth.cs
+++ OnlineTrainingWeb/App_Start/Startup.Auth.cs
@@ -1,11 +1,11 @@
 ﻿using System;
+using Data;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.Owin;
 using Microsoft.Owin;
 using Microsoft.Owin.Security.Cookies;
-using Microsoft.Owin.Security.Google;
+using Model;
 using Owin;
-using OnlineTrainingWeb.Models;
 
 namespace OnlineTrainingWeb
 {

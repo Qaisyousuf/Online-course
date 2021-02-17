86f3b8d2b34653d92372073e3dd3b9648c2a616e
Qais Yousuf <mr.qais.yousuf@gmail.com>
2021-02-17 12:00:47 +0100

    TrainerSocialMediaCreated

---
+++ ViewModel/TrainerSocialMediaViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ViewModel
+{
+    public class TrainerSocialMediaViewModel
+    {
+        public int Id { get; set; }
+
+
+        [Required]
+        [Display(Name ="Profile Url")]
+        public string SocialMediaProfileUrl { get; set; }
+
+        [Required]
+        [Display(Name = "Profile Icon Url")]
+        public string SocialMediaIconUrl { get; set; }
+    }
+}

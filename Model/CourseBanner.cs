26eceadc21225a044ef034b36dc5934dfd941154
Qais Yousuf <mr.qais.yousuf@gmail.com>
2021-02-17 17:04:23 +0100

    CourseBannerCreeated

---
+++ Model/CourseBanner.cs
@@ -0,0 +1,13 @@
+﻿namespace Model
+{
+    public class CourseBanner:EntityBase
+    {
+        public string MainTitle { get; set; }
+        public string SubTitle { get; set; }
+        public string ImageUrl { get; set; }
+        public string JoinButton { get; set; }
+        public string JoinButtonUrl { get; set; }
+        public string DiscoverButton { get; set; }
+        public string DiscoverButtonTUrl { get; set; }
+    }
+}
---
+++ ViewModel/CourseBannerViewModel.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ViewModel
+{
+    public class CourseBannerViewModel
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [Display(Name ="Main Title")]
+        public string MainTitle { get; set; }
+
+        [Required]
+        [Display(Name ="Sub Title")]
+        public string SubTitle { get; set; }
+
+        [Required]
+        [Display(Name ="Image Url")]
+        [DataType(DataType.Url)]
+        public string ImageUrl { get; set; }
+
+        [Required]
+        [Display(Name ="Join Button")]
+        public string JoinButton { get; set; }
+
+        [Required]
+        [Display(Name ="join Button Url")]
+        public string JoinButtonUrl { get; set; }
+
+        [Required]
+        [Display(Name ="Discover Button")]
+        public string DiscoverButton { get; set; }
+
+        [Required]
+        [Display(Name ="Discover url")]
+        public string DiscoverButtonTUrl { get; set; }
+    }
+}

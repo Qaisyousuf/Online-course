a80ca73cb33bba99155cd69397f1bd268b9d00cf
Qais Yousuf <mr.qais.yousuf@gmail.com>
2021-02-17 11:56:10 +0100

    TrainerVisionCreated

---
+++ Model/TrainerVision.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class TrainerVision:EntityBase
+    {
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public string ProfileImageUrl { get; set; }
+        public string TrainerName { get; set; }
+        public string BestRegards { get; set; }
+
+    }
+}
---
+++ ViewModel/TrainerVisionViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ViewModel
+{
+    public class TrainerVisionViewModel
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+
+        [Required]
+        public string Content { get; set; }
+
+        [Required]
+        [Display(Name ="Profile Image Url")]
+        [DataType(DataType.Url)]
+        public string ProfileImageUrl { get; set; }
+
+        [Required]
+        [Display(Name ="Trainer Name")]
+        public string TrainerName { get; set; }
+
+        [Required]
+        [Display(Name = "Best Regards")]
+        public string BestRegards { get; set; }
+    }
+}

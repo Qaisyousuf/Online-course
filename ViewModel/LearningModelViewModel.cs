0c21c1a619bcca758563af8cc1ac0506baef6a62
Qais Yousuf <mr.qais.yousuf@gmail.com>
2021-02-19 23:28:50 +0100

    LearningModelCreated

---
+++ ViewModel/LearningModelViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ViewModel
+{
+    public class LearningModelViewModel
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [Display(Name ="Main Title")]
+        public string MainTitle { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+
+        [Required]
+        public string Content { get; set; }
+
+        [Required]
+        [Display(Name ="Icon url")]
+        [DataType(DataType.Url)]
+        public string IconUrl { get; set; }
+    }
+}

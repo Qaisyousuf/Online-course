f5a9146a9a077117cb8e7e36775a475ff56dcb85
Qais Yousuf <mr.qais.yousuf@gmail.com>
2021-02-19 16:46:47 +0100

    CourseDetailsCreated

---
+++ Model/CourseDetails.cs
@@ -0,0 +1,19 @@
+﻿namespace Model
+{
+    public class CourseDetails:EntityBase
+    {
+        public string MainTitle { get; set; }
+        public string Title { get; set; }
+        public string Duration { get; set; }
+        public string Language { get; set; }
+        public string ProgramName { get; set; }
+        public string VideoLanguage { get; set; }
+        public string Trainer { get; set; }
+        public string ImageUrl { get; set; }
+        public string Certification { get; set; }
+        public string Level { get; set; }
+        public string Availability { get; set; }
+        public string SutdentFinished { get; set; }
+
+    }
+}
---
+++ ViewModel/CourseDetailsViewModel.cs
@@ -0,0 +1,53 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ViewModel
+{
+    public class CourseDetailsViewModel
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [Display(Name ="Main Title")]
+        public string MainTitle { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+
+        [Required]
+        public string Duration { get; set; }
+
+        [Required]
+        public string Language { get; set; }
+
+        [Required]
+        [Display(Name ="Course Title")]
+        public string ProgramName { get; set; }
+
+        [Required]
+        [Display(Name ="Videio Language")]
+        public string VideoLanguage { get; set; }
+
+        [Required]
+        [Display(Name ="Trainer Name")]
+        public string Trainer { get; set; }
+
+        [Required]
+        [Display(Name ="Image Url")]
+        [DataType(DataType.Url)]
+        public string ImageUrl { get; set; }
+
+        [Required]
+        [Display(Name ="Certification")]
+        public string Certification { get; set; }
+
+        [Required]
+        public string Level { get; set; }
+
+        [Required]
+        public string Availability { get; set; }
+
+        [Required]
+        [Display(Name ="Students Finished")]
+        public string SutdentFinished { get; set; }
+    }
+}

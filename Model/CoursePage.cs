5021a201994e911ac7613aba0fe8487dac4f5305
Qais Yousuf <mr.qais.yousuf@gmail.com>
2021-02-21 22:29:22 +0100

    CoursePageRelationIsCreated

---
+++ Model/CoursePage.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace Model
+{
+    public class CoursePage:EntityBase
+    {
+        public string Title { get; set; }
+        public string Slug { get; set; }
+        public string Content { get; set; }
+
+
+        [Display(Name ="Banner")]
+        public int CourseBannerId { get; set; }
+
+        [ForeignKey("CourseBannerId")]
+        public CourseBanner CourseBanner { get; set; }
+
+    }
+}
---
+++ ViewModel/CoursePageViewModel.cs
@@ -0,0 +1,23 @@
+﻿using Model;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ViewModel
+{
+    public class CoursePageViewModel
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+        public string Slug { get; set; }
+        public string Content { get; set; }
+
+
+        [Display(Name = "Banner")]
+        public int CourseBannerId { get; set; }
+
+        [ForeignKey("CourseBannerId")]
+        public CourseBanner CourseBanner { get; set; }
+
+    }
+}

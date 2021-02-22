2b47d583c65770e2bad6f9b0bdda2f4052904b41
Qais Yousuf <mr.qais.yousuf@gmail.com>
2021-02-22 06:31:41 +0100

    ContactDetailCreated

---
+++ Model/ContactDetails.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Model
+{
+    public class ContactDetails:EntityBase
+    {
+        public string MainTitle { get; set; }
+        public string Title { get; set; }
+        public string MobileNumber { get; set; }
+        public string Email { get; set; }
+        public DateTime WorkingStartDate { get; set; }
+        public DateTime WrokingEndDate { get; set; }
+        public string Address { get; set; }
+
+    }
+}
---
+++ ViewModel/ContactDetailsViewModel.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace ViewModel
+{
+    public class ContactDetailsViewModel
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [Display(Name ="Main Title")]
+        public string MainTitle { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+
+        [Display(Name ="Mobile")]
+        [DataType(DataType.PhoneNumber)]
+        [Required]
+        public string MobileNumber { get; set; }
+
+        [Required]
+        [EmailAddress]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+
+        [DataType(DataType.DateTime)]
+        [Display(Name ="Working Start Date")]
+        public DateTime WorkingStartDate { get; set; }
+
+        [DataType(DataType.DateTime)]
+        [Display(Name ="Working End Date")]
+        public DateTime WrokingEndDate { get; set; }
+
+        [Required]
+        public string Address { get; set; }
+    }
+}

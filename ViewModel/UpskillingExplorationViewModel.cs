6499c986b1470f0986fea431b0ea01ffa6aa079d
Qais Yousuf <mr.qais.yousuf@gmail.com>
2021-02-25 11:42:20 +0100

    UpskillingExploration

---
+++ ViewModel/UpskillingExplorationViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ViewModel
+{
+    public class UpskillingExplorationViewModel
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [Display(Name ="Main Title")]
+        public string MainTitle { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+
+        [Required]
+        [Display(Name ="Animation Url")]
+        [DataType(DataType.Url)]
+        public string AnimationUrl { get; set; }
+
+        [Required]
+        public string Content { get; set; }
+
+        [Required]
+        [Display(Name ="Icon Url")]
+        [DataType(DataType.Url)]
+        public string IconUlr { get; set; }
+
+    }
+}

ca3d1c4bcc62ba594633191dd2dc0b30366df488
Qais Yousuf <mr.qais.yousuf@gmail.com>
2021-02-25 14:12:16 +0100

    TrustedbyForUpSkilling

---
+++ Model/TrustedbyForUpSkilling.cs
@@ -0,0 +1,11 @@
+﻿namespace Model
+{
+    public class TrustedbyForUpSkilling:EntityBase
+    {
+        public string MainTitle { get; set; }
+        public string Title { get; set; }
+        public string AnimationUrl { get; set; }
+        public string LogoUrl { get; set; }
+       
+    }
+}
---
+++ ViewModel/TrustedbyForUpSkillingViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ViewModel
+{
+    public class TrustedbyForUpSkillingViewModel
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [Display(Name ="Main title")]
+        public string MainTitle { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+
+        [Required]
+        [Display(Name ="Animation url")]
+        [DataType(DataType.Url)]
+        public string AnimationUrl { get; set; }
+
+        [Required]
+        [Display(Name ="Logo Url")]
+        [DataType(DataType.Url)]
+        public string LogoUrl { get; set; }
+    }
+}

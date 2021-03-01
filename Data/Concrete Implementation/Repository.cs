05236e15f81f812a319988578490d92b3189ef64
Qais Yousuf <mr.qais.yousuf@gmail.com>
2021-03-01 19:04:52 +0100

    IRepositoryCreated

---
+++ Data/Concrete Implementation/Repository.cs
@@ -0,0 +1,52 @@
+﻿using Data.Interfaces;
+using System;
+using System.Linq;
+using System.Data.Entity;
+
+namespace Data.Concrete_Implementation
+{
+    public abstract class Repository<TModel> : IRepository<TModel> where TModel : class
+    {
+        protected ApplicationDbContext _context;
+
+        public Repository(ApplicationDbContext Context)
+        {
+            _context = Context;
+        }
+        public void Add(TModel entity)
+        {
+            _context.Set<TModel>().Add(entity);
+        }
+
+        public IQueryable<TModel> GetAll()
+        {
+            var data = _context.Set<TModel>();
+
+            return data;
+        }
+
+        public TModel GetById(object Id)
+        {
+            return _context.Set<TModel>().Find(Id);
+        }
+
+        public void Remove(TModel entity)
+        {
+            _context.Set<TModel>().Remove(entity);
+        }
+
+        public void RemoveById(object Id)
+        {
+            TModel model = GetById(Id);
+
+            if (model != null)
+                Remove(model);
+        }
+
+        public void Update(TModel entity)
+        {
+            _context.Entry(entity).State =EntityState.Modified;
+
+        }
+    }
+}
---
+++ Data/Interfaces/IRepository.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Interfaces
+{
+    public interface IRepository<TModel> where TModel:class
+    {
+        void Add(TModel entity);
+
+        void Update(TModel entity);
+
+        IQueryable<TModel> GetAll();
+
+        TModel GetById(object Id);
+
+        void Remove(TModel entity);
+
+        void RemoveById(object Id);
+    }
+}

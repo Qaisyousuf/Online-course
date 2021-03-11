5779194bc94d8bfbb6d9a86844a8346d7716ffff
Qais Yousuf <mr.qais.yousuf@gmail.com>
2021-03-11 15:23:29 +0100

    UnityInstalled

---
+++ OnlineTrainingWeb/Areas/ALOTAdmin/ALOTAdminAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace OnlineTrainingWeb.Areas.ALOTAdmin
+{
+    public class ALOTAdminAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "ALOTAdmin";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "ALOTAdmin_default",
+                "ALOTAdmin/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
---
+++ OnlineTrainingWeb/Areas/ALOTAdmin/Controllers/HomeBannerController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using OnlineTrainingWeb.ViewModel;
+using Data.Interfaces;
+using ViewModel;
+
+namespace OnlineTrainingWeb.Areas.ALOTAdmin.Controllers
+{
+    public class HomeBannerController : Controller
+    {
+        private readonly IUnitOfWork uow;
+
+        public HomeBannerController(IUnitOfWork uow)
+        {
+            this.uow = uow;
+        }
+        public ActionResult Index()
+        {
+           var homebanner= uow.HomeBannerRepository.GetAll();
+
+            List<HomeBannerViewModel> viewmodel = new List<HomeBannerViewModel>();
+
+
+            foreach (var item in homebanner)
+            {
+                viewmodel.Add(new HomeBannerViewModel
+                {
+                    Id=item.Id,
+                    MainTitle=item.MainTitle,
+                    SubTitle=item.SubTitle,
+                    ImageUrl=item.ImageUrl,
+                    JoinButton=item.JoinButton,
+                    JoinButtonUrl=item.JoinButtonUrl,
+                    DiscoverButton=item.DiscoverButton,
+                    DiscoverButtonUrl=item.DiscoverButton,
+
+
+                });
+            }
+            return View(viewmodel);
+        }
+    }
+}

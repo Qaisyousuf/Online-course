2d00b9a0800d4e9fc939eef3bf5c5e8ff42b7a46
Qais Yousuf <mr.qais.yousuf@gmail.com>
2021-03-11 14:52:52 +0100

    IUnitOfWorkCreated

---
+++ Data/Concrete Implementation/HomeBannerRepository.cs
@@ -0,0 +1,18 @@
+﻿using Data.Interfaces;
+using Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Concrete_Implementation
+{
+    public class HomeBannerRepository:Repository<HomeBanner>,IHomeBannerRepository
+    {
+        public HomeBannerRepository(ApplicationDbContext context):base(context)
+        {
+
+        }
+    }
+}
---
+++ Data/Concrete Implementation/UnitOfWork.cs
@@ -0,0 +1,28 @@
+﻿using Data.Interfaces;
+
+namespace Data.Concrete_Implementation
+{
+    public class UnitOfWork:IUnitOfWork
+    {
+        public ApplicationDbContext Context { get; }
+        public UnitOfWork()
+        {
+            Context = new ApplicationDbContext();
+        }
+
+        public IHomeBannerRepository HomeBannerRepository
+        {
+            get { return new HomeBannerRepository(Context); }
+        }
+
+        public void Commit()
+        {
+            Context.SaveChanges();
+        }
+
+        public void Dispose()
+        {
+            Context.Dispose();
+        }
+    }
+}

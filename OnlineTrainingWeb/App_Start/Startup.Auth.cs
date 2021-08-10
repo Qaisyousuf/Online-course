729871b24373b0c7e6208f2136e0a3ac43e4bf24
Qais Yousuf <mr.qais.yousuf@gmail.com>
2021-08-10 10:40:23 +0200

    TwitterAuthenticationIsCreated

--- OnlineTrainingWeb/App_Start/Startup.Auth.cs
+++ OnlineTrainingWeb/App_Start/Startup.Auth.cs
@@ -5,8 +5,10 @@ using Microsoft.AspNet.Identity.Owin;
 using Microsoft.Owin;
 using Microsoft.Owin.Security.Cookies;
 using Microsoft.Owin.Security.Google;
+using Microsoft.Owin.Security.Twitter;
 using Models;
 using Owin;
+
 
 namespace OnlineTrainingWeb
 {
@@ -50,10 +52,26 @@ namespace OnlineTrainingWeb
             //app.UseMicrosoftAccountAuthentication(
             //    clientId: "",
             //    clientSecret: "");
+            //app.UseTwitterAuthentication(new TwitterAuthenticationOptions
+            //{
+            //    ConsumerKey = "906320754303815680-ByG4GttnSAQXM2MIR3jn7sqD3GxWEdj",
+            //    ConsumerSecret = "16agWcHBTpQApbxeqywTf2kx6bnPBezpeZ80s3H0iPloP",
+            //    BackchannelCertificateValidator = new Microsoft.Owin.Security.CertificateSubjectKeyIdentifierValidator(new[]
+            //{
 
-            //app.UseTwitterAuthentication(
-            //   consumerKey: "",
-            //   consumerSecret: "");
+            //      "A5EF0B11CEC04103A34A659048B21CE0572D7D47", // VeriSign Class 3 Secure Server CA - G2
+            //      "0D445C165344C1827E1D20AB25F40163D8BE79A5", // VeriSign Class 3 Secure Server CA - G3
+            //      "7FD365A7C2DDECBBF03009F34339FA02AF333133", // VeriSign Class 3 Public Primary CA - G5
+            //      "39A55D933676616E73A761DFA16A7E59CDE66FAD", // Symantec Class 3 Secure Server CA - G4
+            //      "‎add53f6680fe66e383cbac3e60922e3b4c412bed", // Symantec Class 3 EV SSL CA - G3
+            //      "4eb6d578499b1ccf5f581ead56be3d9b6744a5e5", // VeriSign Class 3 Primary CA - G5
+            //      "5168FF90AF0207753CCCD9656462A212B859723B", // DigiCert SHA2 High Assurance Server C‎A 
+            //      "B13EC36903F8BF4701D498261A0802EF63642BC3"  // DigiCert High Assurance EV Root CA
+            //     })
+            //});
+            app.UseTwitterAuthentication(
+               consumerKey: "b04YFUIzF1GnNLy3uKWwQvH6j",
+               consumerSecret: "oqfMQMZMWktQ7VUEbRUAru6Dfq1ZrRTAbyqIRUMnJ3677Z7PRc");
 
             app.UseFacebookAuthentication(
                appId: "900313414029603",
--- OnlineTrainingWeb/Global.asax.cs
+++ OnlineTrainingWeb/Global.asax.cs
@@ -17,5 +17,6 @@ namespace OnlineTrainingWeb
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
+
     }
 }
